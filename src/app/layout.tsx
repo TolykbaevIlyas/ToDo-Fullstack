cab7f7ed2e723c31a94fab56f1efabf5f88e3b89
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-20 20:46:23 +0600

    Change Imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/widgets/Header/ui/Header";
+import { Header } from "@/widgets/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
