4592c3f357a495266c2d1cdfa288d6027dff5b77
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-20 20:44:01 +0600

    Create Header

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from "react"
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+        Header
+    </div>
+  )
+}
+
+export default Header
